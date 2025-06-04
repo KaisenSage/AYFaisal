936ba3806c591eacc2c8400d789a9755fb899861
Kaisensage <kvngbabstar@gmail.com>
2025-06-04 16:50:26 +0100

    commit4thjune

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -5,10 +5,10 @@ export const Footer = () => (
     <div className="flex flex-col px-0 py-2 w-screen
       sm:flex-row sm:justify-between sm:items-center">
       <div className="text-[#eccec6] text-xs text-center sm:text-left sm:text-sm font-medium flex-1 px-3">
-        © {new Date().getFullYear()} SageTech Development Services. All rights reserved.
+        © {new Date().getFullYear()} Sagecorp Development Services. All rights reserved.
       </div>
       <div className="text-[#eccec6] text-xs text-center sm:text-right sm:text-sm font-medium flex-1 px-3 mt-2 sm:mt-0">
-        Designed &amp; built by SageTech Development Services
+        Designed &amp; built by Sagecorp Development Services
       </div>
     </div>
   </footer>
