8e283f7ab03cd9ac220cd9bc4d677a025085a578
Kaisensage <kvngbabstar@gmail.com>
2025-06-04 15:25:43 +0100

    commit4thjune

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 export const Footer = () => (
-  <footer className="fixed bottom-0 left-0 w-full z-50 bg-black">
-    <div className="flex flex-col items-center px-3 py-2 w-full
+  <footer className="w-screen bg-black">
+    <div className="flex flex-col px-0 py-2 w-screen
       sm:flex-row sm:justify-between sm:items-center">
-      <span className="text-[#eccec6] text-xs text-center sm:text-left sm:text-sm font-medium">
+      <div className="text-[#eccec6] text-xs text-center sm:text-left sm:text-sm font-medium flex-1 px-3">
         © {new Date().getFullYear()} Sagecorp Development Services. All rights reserved.
-      </span>
-      <span className="text-[#eccec6] text-xs text-center sm:text-right sm:text-sm">
+      </div>
+      <div className="text-[#eccec6] text-xs text-center sm:text-right sm:text-sm font-medium flex-1 px-3 mt-2 sm:mt-0">
         Designed &amp; built by Sagecorp Development Services
-      </span>
+      </div>
     </div>
   </footer>
 );
