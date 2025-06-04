796c5c2b9afab49ca172abcf36df1f05f4099f82
Kaisensage <kvngbabstar@gmail.com>
2025-06-04 14:34:42 +0100

    commit4thjune

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const Footer = () => (
-  <footer className="fixed bottom-0 left-0 w-full z-50 bg-black">
+  <footer className=" bottom-0 left-0 w-full z-50 bg-black">
     <div className="flex flex-col items-center px-3 py-2 w-full
       sm:flex-row sm:justify-between sm:items-center">
       <span className="text-[#eccec6] text-xs text-center sm:text-left sm:text-sm font-medium">
